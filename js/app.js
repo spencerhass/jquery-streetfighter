aca74cc5a9eba6145d5e38ea1922349a425636e4
spencerhass <shass615@yahoo.com>
2015-09-13 23:39:40 -0400

    changes to js file and hmtl

--- js/app.js
+++ js/app.js
@@ -5,17 +5,51 @@ $(document).ready(function() {
     $('.ryu-ready').show();
   })
   .mouseleave(function() {
-    $('.ryu-ready').hide();
     $('.ryu-still').show();
-  });
+    $('.ryu-ready').hide();
+  })
+  .mousedown(function(){
+  playHadouken();
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show().animate(
+    {'left':'1020px'},
+    500,
+    function(){
+    $(this).hide();
+    $(this).css ('left','520px');
+  })
+  })
+  
+.mouseup(function(){
+$('.ryu-throwing').hide();
+$('.ryu-ready').show();
 });
 
-.mousedown(function(){
-$('.ryu-ready').hide();
-$('.ryu-still').show();
-$('.hadouken').show();
+});
+
+function playHadouken() {
+        $('#hadouken-sound')[0].volume = 0.5;
+        $('#hadouken-sound')[0].load();
+        $('#hadouken-sound')[0].play();
+    };
+ 
+$(document).keydown(function(e) {
+		if (e.keyCode == 88) {
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+            $('.ryu-throwing').hide();
+			$('.ryu-cool').show();
+			$('.hadouken').hide();
+		}
 	})
+	$(document).keyup(function(e) {
+		if (e.keyCode == 88) {
+			$('.ryu-still').show();
+			$('.ryu-ready').show();
+			$('.ryu-cool').hide();
+            $('.ryu-throwing').show();
+			$('.hadouken').show();
 
-
-  	
-	
+		}
+		});
